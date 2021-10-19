ba841e1dbb9108693199c3c158a973a110993ee4
sam bacha <sam@manifoldfinance.com>
2021-10-19 16:56:01 -0700

    feat(react): uniswap skeleton

---
+++ src/pages/App.tsx
@@ -0,0 +1,76 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import GoogleAnalyticsReporter from '../components/analytics/GoogleAnalyticsReporter';
+import Header from '../components/Header';
+import Polling from '../components/Header/Polling';
+import URLWarning from '../components/Header/URLWarning';
+import Popups from '../components/Popups';
+import Web3ReactManager from '../components/Web3ReactManager';
+import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader';
+import Home from './home';
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  overflow-x: hidden;
+`;
+
+const HeaderWrapper = styled.div`
+  ${({ theme }) => theme.flexRowNoWrap}
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 100px;
+  align-items: center;
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 10;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    padding: 16px;
+    padding-top: 2rem;
+  `};
+
+  z-index: 1;
+`;
+
+const Marginer = styled.div`
+  margin-top: 5rem;
+`;
+
+// fill in if any top level modals needed
+// function TopLevelModals() {}
+
+export default function App() {
+  return (
+    <Suspense fallback={null}>
+      <Route component={GoogleAnalyticsReporter} />
+      <Route component={DarkModeQueryParamReader} />
+      <AppWrapper>
+        <URLWarning />
+        <HeaderWrapper>
+          <Header />
+        </HeaderWrapper>
+        <BodyWrapper>
+          <Popups />
+          <Polling />
+          {/* <TopLevelModals /> */}
+          <Web3ReactManager>
+            <Switch>
+              <Route exact strict path="/" component={Home} />
+            </Switch>
+          </Web3ReactManager>
+          <Marginer />
+        </BodyWrapper>
+      </AppWrapper>
+    </Suspense>
+  );
+}
